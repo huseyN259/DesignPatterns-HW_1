35b64a46d7ec0f6fc8ae1e070c18a2104dcdc0e5
user <user@WINDOWS-OM6HICP>
2022-11-16 11:16:42 +0400

    Add project files.

---
+++ DP-HW_1/HW_1_1.cs
@@ -0,0 +1,159 @@
+﻿using static System.Console;
+
+interface IChair
+{
+	void HasLegs();
+	void SitOn();
+}
+
+class VictorianChair : IChair
+{
+	public void HasLegs()
+		=> WriteLine($"Has Legs {nameof(VictorianChair)}...");
+
+	public void SitOn()
+		=> WriteLine($"Sit On {nameof(VictorianChair)}...");
+}
+
+class ModernChair : IChair
+{
+	public void HasLegs()
+		=> WriteLine($"Has No Legs{nameof(ModernChair)}...");
+	public void SitOn() 
+		=> WriteLine($"Sit on {nameof(ModernChair)}...");
+}
+
+class ArtDecoChair : IChair
+{
+	public void HasLegs() 
+		=> WriteLine($"Has Legs {nameof(ArtDecoChair)}...");
+	public void SitOn() 
+		=> WriteLine($"Sit On {nameof(ArtDecoChair)}...");
+}
+
+interface ISofa
+{
+	void HasLegs();
+	void SitOn();
+}
+
+class VictorianSofa : ISofa
+{
+	public void HasLegs() 
+		=> WriteLine($"Has Legs {nameof(VictorianSofa)}...");
+	public void SitOn() 
+		=> WriteLine($"Sit On {nameof(VictorianSofa)}...");
+}
+
+class ModernSofa : ISofa
+{
+	public void HasLegs() 
+		=> WriteLine($"Has No Legs {nameof(ModernSofa)}...");
+	public void SitOn() 
+		=> WriteLine($"Sit On {nameof(ModernSofa)}...");
+}
+
+class ArtDecoSofa : ISofa
+{
+	public void HasLegs() 
+		=> WriteLine($"Has Legs {nameof(ArtDecoSofa)}...");
+	public void SitOn() 
+		=> WriteLine($"Sit On {nameof(ArtDecoSofa)}...");
+}
+
+interface ICoffeeTable
+{
+	void IsRound();
+	void PlaceOn();
+}
+
+class VictorianCoffeeTable : ICoffeeTable
+{
+	public void IsRound() 
+		=> WriteLine($"Is Round {nameof(VictorianCoffeeTable)}...");
+	public void PlaceOn() 
+		=> WriteLine($"Placed On {nameof(VictorianCoffeeTable)}...");
+}
+
+class ModernCoffeeTable : ICoffeeTable
+{
+	public void IsRound() 
+		=> WriteLine($"Is Round {nameof(ModernCoffeeTable)}...");
+	public void PlaceOn() 
+		=> WriteLine($"Placed On {nameof(ModernCoffeeTable)}...");
+}
+
+class ArtDecoCoffeeTable : ICoffeeTable
+{
+	public void IsRound() => 
+		WriteLine($"Is Not Round {nameof(ArtDecoCoffeeTable)}...");
+	public void PlaceOn() => 
+		WriteLine($"Placed On {nameof(ArtDecoCoffeeTable)}...");
+}
+
+abstract class FurnitureFactory
+{
+	public abstract IChair CreateChair();
+	public abstract ISofa CreateSofa();
+	public abstract ICoffeeTable CreateCoffeeTable();
+}
+
+class VictorianFurnitureFactory : FurnitureFactory
+{
+	public override IChair CreateChair() 
+		=> new VictorianChair();
+
+	public override ICoffeeTable CreateCoffeeTable() 
+		=> new VictorianCoffeeTable();
+
+	public override ISofa CreateSofa() 
+		=> new VictorianSofa();
+}
+
+class ModernFurnitureFactory : FurnitureFactory
+{
+	public override IChair CreateChair() 
+		=> new ModernChair();
+
+	public override ICoffeeTable CreateCoffeeTable() 
+		=> new ModernCoffeeTable();
+
+	public override ISofa CreateSofa() 
+		=> new ModernSofa();
+}
+
+class ArtDecoFurnitureFactory : FurnitureFactory
+{
+	public override IChair CreateChair() 
+		=> new ArtDecoChair();
+
+	public override ICoffeeTable CreateCoffeeTable() 
+		=> new ArtDecoCoffeeTable();
+
+	public override ISofa CreateSofa() 
+		=> new ArtDecoSofa();
+}
+
+class Client
+{
+	public void Run()
+	{
+		FurnitureFactory furnitureFactory = new ModernFurnitureFactory();
+		furnitureFactory.CreateSofa().SitOn();
+
+		furnitureFactory = new ArtDecoFurnitureFactory();
+		furnitureFactory.CreateCoffeeTable().IsRound();
+
+		furnitureFactory = new VictorianFurnitureFactory();
+		furnitureFactory.CreateChair().HasLegs();
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		Client client = new Client();
+		client.Run();
+	}
+}
---
+++ HW_1_2/HW_1_2.cs
@@ -0,0 +1,68 @@
+﻿using static System.Console;
+
+interface IButton
+{
+	void Render();
+	void OnClick();
+}
+
+class WindowsButton : IButton
+{
+	public void OnClick() 
+		=> WriteLine("Windows Button Click...");
+	public void Render() 
+		=> WriteLine("Windows Button Render...");
+}
+
+class HTMLButton : IButton
+{
+	public void OnClick()
+		=> WriteLine("HTML Button Click...");
+	public void Render()
+		=> WriteLine("HTML Button Render...");
+}
+
+abstract class Dialog
+{
+	public void Render()
+	{
+		IButton button = CreateButton();
+		button.OnClick();
+		button.Render();
+	}
+
+	public abstract IButton CreateButton();
+}
+
+class WindowsDialog : Dialog
+{
+	public override IButton CreateButton()
+		=> new WindowsButton();
+}
+
+class WebDialog : Dialog
+{
+	public override IButton CreateButton()
+		=> new HTMLButton();
+}
+
+class Client
+{
+	public void Run()
+	{
+		Dialog dialog = new WindowsDialog();
+		dialog.Render();
+
+		dialog = new WebDialog();
+		dialog.Render();
+	}
+}
+
+class HW_1_2
+{
+	static void Main()
+	{
+		Client client = new Client();
+		client.Run();
+	}
+}
